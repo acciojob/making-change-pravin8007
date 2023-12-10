977be49ac2d3215084cad1706bb0c7fccb0b20a4
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-12-10 12:49:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const makeChange = (c) => {
-  // your name here
+    if (c <= 0 || c >= 100) {
+        throw new Error("Invalid input. Amount should be a positive integer less than 100 and greater than 0.");
+    }
+
+    const quarters = Math.floor(c / 25);
+    const dimes = Math.floor((c % 25) / 10);
+    const nickels = Math.floor(((c % 25) % 10) / 5);
+    const pennies = ((c % 25) % 10) % 5;
+
+    return { "q": quarters, "d": dimes, "n": nickels, "p": pennies };
 };
 
-// Do not the change the code below
+// Do not change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
